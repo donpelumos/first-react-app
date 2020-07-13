79e6ac4134841ad9ae8713805918544aa6fef711
donpelumos <donpelumos@gmail.com>
2020-07-13 15:05:20 +0100

    implemented passing of event from child to parent component

--- src/components/CounterComponent.jsx
+++ src/components/CounterComponent.jsx
@@ -2,13 +2,9 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: this.props.value,
   };
 
-  constructor() {
-    super();
-    this.handleButtonClick = this.handleButtonClick.bind(this);
-  }
   render() {
     return (
       <div>
@@ -19,9 +15,17 @@ class Counter extends Component {
           onClick={() => {
             this.handleButtonClick("pelumi");
           }}
-          className="btn btn-primary"
+          className="btn btn-primary m-2"
         >
           click me
+        </button>
+        <button
+          onClick={() => {
+            this.props.onDelete(this.props.id);
+          }}
+          className="btn btn-primary btn-danger m-2"
+        >
+          Delete Button
         </button>
       </div>
     );
@@ -36,10 +40,10 @@ class Counter extends Component {
     );
   }
 
-  handleButtonClick(arg) {
+  handleButtonClick = (arg) => {
     console.log(arg);
     this.setState({ count: this.state.count + 1 });
-  }
+  };
 }
 
 export default Counter;
--- src/components/CounterListComponent.jsx
+++ src/components/CounterListComponent.jsx
@@ -5,7 +5,7 @@ class CounterList extends Component {
   state = {
     counters: [
       { id: 1, value: 0 },
-      { id: 2, value: 0 },
+      { id: 2, value: 5 },
       { id: 3, value: 0 },
     ],
   };
@@ -13,11 +13,24 @@ class CounterList extends Component {
     return (
       <React.Fragment>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} />
+          <Counter
+            key={counter.id}
+            value={counter.value}
+            id={counter.id}
+            onDelete={this.deleteCounter}
+          />
         ))}
       </React.Fragment>
     );
   }
+
+  deleteCounter = (counterId) => {
+    console.log("Delete counter called - " + counterId);
+    let counters = this.state.counters.filter(
+      (counter) => counter.id !== counterId
+    );
+    this.setState({ counters: counters });
+  };
 }
 
 export default CounterList;
